23de8c1114effe807d8ee3f56d01a0bba66e1ccd
chac019 <chac019@hotmail.com>
2015-06-22 15:29:01 +0200

    page formulaire
    voir object

---
+++ Formulaire.php
@@ -0,0 +1,41 @@
+<!Doctype html>
+<html>
+<head>
+    <title>TP</title>
+    <!-- metas -->
+    <meta charset="UTF-8" />
+    <!-- styles -->
+    <link rel="stylesheet" href="Css/index.css" />
+    <!-- scripts -->
+    <script src="Js/index.js" >
+    </script>
+</head>
+<body>
+<div class="myclass">
+
+    <form action="result.php" method="get">
+        <table>
+            <tbody>
+            <tr>
+                <td> Nom:</td>
+                <td> <input type="text" name="name" placeholder="nom" /></td>
+            </tr>
+            <tr>
+                <td> e-mail:</td>
+                <td> <input type="text" name="mail" placeholder="email" /></td>
+            </tr>
+            <tr>
+                <td> Mot de passe:</td>
+                <td> <input type="text" name="password" placeholder="motdepasse" /></td>
+            </tr>
+
+            <tr>
+                <td> <input type="submit" value="Envoyer" /> </td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
+
+</div>
+</body>
+</html>
